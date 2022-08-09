ff40820e961464da8ff99e50c2368dcb311c76de
sourav <souravmahanty12@gmail.com>
2022-08-09 17:23:56 +0530

    done

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import "./App.css";
+// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+function App() {
+  return (
+    <div className="main">
+      <div className="App">
+        <div className="leaf">
+          <div className="leaf-sub1">
+            <div className="leaf1"></div>
+            <div className="leaf2"></div>
+          </div>
+          <div className="leaf-sub2">
+            <div className="leaf3"></div>
+            <div className="leaf4"></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
